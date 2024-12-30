097e2737cb80125a9632d21224238176a105f2e7
Gautam <159130133+CookieCums@users.noreply.github.com>
2024-12-30 09:12:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def send_message(text):
         print("Message sent successfully: ", text)
     with client:
         client.loop.run_until_complete(main())
-      
+
 def monitor_clipboard():
     global last_sent_text
   
@@ -31,13 +31,16 @@ def monitor_clipboard():
         try:
             current_text = pyperclip.paste()
             if current_text and current_text != last_sent_text:
-                if re.match(instagram_reel_pattern, current_text):
+                match = re.match(instagram_reel_pattern, current_text)
+                if match:
                     last_sent_text = current_text
-                    send_message(current_text)
+                    # found an error if the linkes contains 'Reels' insted of 'reel' the instagram doesnt allow you to watch reel and asks you to login in
+                    modified_text = current_text.replace("reels", "reel")
+                    send_message(modified_text)
             time.sleep(1)
         except Exception as e:
             print("An error occurred:", e)
-          
+
 if __name__ == "__main__":
     print("Monitoring clipboard for new copied text...")
-    monitor_clipboard()  
+    monitor_clipboard()
